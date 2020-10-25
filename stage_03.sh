50b625854297cadec74482237d798af7934aa4e3
Alexander Strebkov <a.strebkov@frontol.ru>
2020-10-25 21:41:20 +0400

    Disable auto screen lock

--- stage_03.sh
+++ stage_03.sh
@@ -4,3 +4,8 @@
 # Enable Breeze-Dark in plasma 
 lookandfeeltool --apply org.kde.breezedark.desktop
 
+# Disable auto screenlocking
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key Autolock --type bool false
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key LockOnResume --type bool false
+
+
